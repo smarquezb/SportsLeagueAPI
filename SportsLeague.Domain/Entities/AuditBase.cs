65f3c84da324e68a221b4ec53c5f885e0a657328
Santiago Marquez betancur <santimb70@gmail.com>
2026-03-11 13:40:26 -0500

    Add project files.

---
+++ SportsLeague.Domain/Entities/AuditBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Entities
+{
+    public abstract class AuditBase
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ SportsLeague.Domain/Entities/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Entities
+{
+    public class Team : AuditBase
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string City { get; set; } = string.Empty;
+
+        public string Stadium { get; set; } = string.Empty;
+
+        public string? LogoUrl { get; set; }
+
+        public DateTime FoundedDate { get; set; }
+    }
+}
