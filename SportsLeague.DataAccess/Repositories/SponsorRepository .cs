558974bb04073d2588f48fb53ffbaad542a8ff51
Santiago Marquez betancur <santimb70@gmail.com>
2026-04-03 20:31:56 -0500

    final changes

--- SportsLeague.DataAccess/Repositories/SponsorRepository .cs
+++ SportsLeague.DataAccess/Repositories/SponsorRepository .cs
@@ -5,17 +5,18 @@ using SportsLeague.Domain.Interfaces.Repositories;
 
 namespace SportsLeague.DataAccess.Repositories
 {
-  
     public class SponsorRepository : GenericRepository<Sponsor>, ISponsorRepository
     {
         // Constructor que inyecta el contexto de base de datos a la clase base
         public SponsorRepository(LeagueDbContext dbContext) : base(dbContext) { }
 
-       
         // Localiza un patrocinador en el sistema filtrando por su nombre comercial.
-    
-        public async Task<Sponsor?> GetByNameAsync(string sponsorName) =>
-            await _dbSet.FirstOrDefaultAsync(s => s.Name.Equals(sponsorName, StringComparison.OrdinalIgnoreCase));
+        public async Task<Sponsor?> GetByNameAsync(string sponsorName)
+        {
+            
+            return await _dbSet.FirstOrDefaultAsync(s => s.Name.ToUpper() == sponsorName.ToUpper());
+        }
     }
 }
+
 
